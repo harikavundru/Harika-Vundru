d3cf7eab35fcf215138cd8d4484448cb3e5c455c
harikavundru <103499261+harikavundru@users.noreply.github.com>
2022-06-16 23:44:43 +0530

    Update avgof5submarks.c

--- avgof5submarks.c
+++ avgof5submarks.c
@@ -11,10 +11,15 @@ Write your code in this editor and press "Run" button to compile and execute it.
 int main()
 {
     int phy,chem,math,cs,eng,sum,avg;
+    printf("Enter Physics marks: ");
     scanf("%d",&phy);
+    printf("Enter Chemistry marks: ");
     scanf("%d",&chem);
+    printf("Enter Mathematics marks: ");
     scanf("%d",&math);
+    printf("Enter CS marks: ");
     scanf("%d",&cs);
+    printf("Enter English marks: ");
     scanf("%d",&eng);
     sum = phy + chem + math + cs + eng;
     avg = sum/5;
